2c7309833909c35c06ba09f329f3a58298013c37
bragosi <oladejoboluwatife20003@gmail.com>
2025-11-25 00:34:38 +0100

    bug fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:8000/api/v1"
-    : "https://gramosi-social-media-app-2.onrender.com/api/v1";
+    : "/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
